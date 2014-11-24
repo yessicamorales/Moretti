db0de495747d70adf775f93f3121a0c5064a6201
Yessica Morales <yessicamoralesreyes@gmail.com>
2014-11-24 03:35:37 -0500

    Added home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def home
+  	@menus = [
+  		{title: 'Categorías', icon:'fa fa-th-large', link: "/categories"},
+  		{title: 'Productos', icon:'fa fa-glass', link: "/products"},
+  		{title: 'Proveedores', icon:'fa fa-truck', link: "/providers"},
+  		{title: 'Ordenes', icon:'fa fa-ticket', link: "/orders"},
+  		{title: 'Ventas', icon:'fa fa-credit-card', link: "/sells"},
+  		{title: 'Compras', icon:'fa fa-shopping-cart', link: "/purchases"},
+  		{title: 'Usuarios', icon:'fa fa-group', link: "/users"},
+  		{title: 'Salir', icon:'fa fa-send', link: "/logout"}
+  	]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Moretti::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
+  root 'application#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
